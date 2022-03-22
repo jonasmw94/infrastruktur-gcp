396a88508f3d606928dcf89d97e6ad2fe705cd92
Jonas Wagle <jonas.mossin@gmail.com>
2022-03-22 13:52:02 +0100

    fix

--- cloud_function/index.js
+++ cloud_function/index.js
@@ -1,17 +1,24 @@
 exports.testPubSub = (message, context) => {
     async function insertRowsAsStream() {
         const { BigQuery } = require("@google-cloud/bigquery");
+        const jwt_decode = require('jwt-decode');
         const bigquery = new BigQuery();
 
-        const rows = [
-            {transaction_id: 'filenameid', transaction_name: "filename"}
-        ];
+        const decoded = jwt_decode(message.data, { header: true });
 
-        await bigquery
+        const rows = [
+            {transaction_id: decoded.md5Hash, transaction_name: decoded.name}
+        ];
+        
+        if (decoded.name.toLowerCase().includes(".pdf")) {
+            await bigquery
             .dataset('infrastructu_tut_bew_wo_2022_dataset')
             .table('transaction')
             .insert(rows);
-            console.log(`Inserted ${rows.length} rows`);
+            console.log(`Inserted ${rows.length} row(s)`);
+        }else {
+            console.log("Ignoring file " + decoded.name)
+        }
     }
 
     insertRowsAsStream();
